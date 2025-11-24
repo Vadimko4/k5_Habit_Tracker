a84076201007daa17aa3199941c9ff3a871fe2bd
Vadimko4 <vad.informat@google.com>
2025-11-24 20:46:32 +0400

    update settings.py, models, add urls for users, make migrations

--- config/settings.py
+++ config/settings.py
@@ -88,9 +88,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-# Internationalization
-# https://docs.djangoproject.com/en/5.2/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
@@ -99,12 +96,11 @@ USE_I18N = True
 
 USE_TZ = True
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.2/howto/static-files/
-
 STATIC_URL = 'static/'
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'users.User'
--- users/models.py
+++ users/models.py
@@ -1,3 +1,28 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+# from materials.models import Course, Lesson
+# from .managers import UserManager
 
-# Create your models here.
+
+class User(AbstractUser):
+    username = None
+
+    email = models.EmailField(unique=True, verbose_name="Почта", help_text="Укажите почту")
+    phone = models.CharField(max_length=35, verbose_name="Телефон", blank=True, null=True,
+                             help_text="Введите номер телефона")
+    town = models.CharField(max_length=50, verbose_name="Город", blank=True, null=True,
+                            help_text="Введите город")
+    avatar = models.ImageField(upload_to="users/avatars/", verbose_name="Аватар", blank=True, null=True,
+                               help_text="Загрузите свой аватар")
+
+    USERNAME_FIELD = "email"  # меняем юзернейм на почту
+    REQUIRED_FIELDS = []
+
+    # objects = UserManager()
+
+    def __str__(self):
+        return f'{self.email}'
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
