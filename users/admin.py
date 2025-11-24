9ef1b73c3c7b313f5fc7c5e501c30578f85edca7
Vadimko4 <vad.informat@google.com>
2025-11-24 22:12:53 +0400

    add superuser, update user model

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+
+from users.models import User
+
+
+@admin.register(User)  # Регистрируем модель
+class UserAdmin(admin.ModelAdmin):
+    # Настраиваем поля, которые будем выводить в админке
+    list_display = ('id', 'email',)
+
+
+# @admin.register(Payment)  # Регистрируем модель
+# class PaymentAdmin(admin.ModelAdmin):
+#     # Настраиваем поля, которые будем выводить в админке
+#     list_display = ('id', 'user', 'amount','course', 'lesson', 'date', 'type')
+#     # По чему будем делать фильтрацию
+#     list_filter = ('id', 'course', 'user')
+#     # По чему у нас будет поиск
+#     search_fields = ('user', 'course')
